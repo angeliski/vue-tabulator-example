4152f6971e44c3efa7aa745c0c4400747e2ed78d
Rogerio Angeliski <angeliski@hotmail.com>
2019-02-07 21:11:06 -0200

    Build an example

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueTabulator from 'vue-tabulator';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueTabulator);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueTabulator ref="tabulator" v-model="data" :options="options"/>
+
+    <button id="row" @click="newRow">New Row</button>
+    <button id="column" @click="newColumn">New Column</button>
+    <button id="clear" @click="clearTable">Clear Table</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: "home",
+  data() {
+    return {
+      data: [{ name: "Teste", age: 13 }],
+      options: {
+        layout:"fitDataFill",
+        columns: [
+          {
+            title: "Name",
+            field: "name",
+            sorter: "string",
+            width: 200,
+            editor: true
+          }
+        ]
+      }
+    };
   },
+  methods: {
+    newRow() {
+      this.data.push({ name: "Teste 2", age: 15 });
+    },
+    newColumn() {
+      this.options.columns.push({
+        title: "age",
+        field: "age",
+        sorter: "number",
+        width: 300,
+        editor: false
+      });
+    },
+    clearTable() {
+      this.$refs.tabulator.getInstance().clearData();
+    }
+  }
 };
 </script>
+
+<style lang='scss'>
+@import "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4";
+.home {
+  height: 100%;
+}
+</style>
